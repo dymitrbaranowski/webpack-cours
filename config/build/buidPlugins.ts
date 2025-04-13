f3e2f27efead619fd1dc1b15b33d1b0cab2374da
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-04-13 18:45:42 +0300

    fox1

--- config/build/buidPlugins.ts
+++ config/build/buidPlugins.ts
@@ -6,6 +6,7 @@ import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import path from "path";
+import CopyPlugin from "copy-webpack-plugin";
 
 export function buildPlugins({
   mode,
@@ -39,6 +40,16 @@ export function buildPlugins({
       new MiniCssExtractPlugin({
         filename: "css/[name].[contenthash:8].css",
         chunkFilename: "css/[name].[contenthash:8].css",
+      })
+    );
+    plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(paths.public, "locales"),
+            to: path.resolve(paths.output, "locales"),
+          },
+        ],
       })
     );
   }
--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -87,5 +87,31 @@ export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
     ],
   };
 
-  return [assetLoader, scssLoader, tsLoader, svgLoader];
+  const babelLoader = {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-typescript",
+          [
+            "@babel/preset-react",
+            {
+              runtime: isDev ? "automatic" : "classic",
+            },
+          ],
+        ],
+      },
+    },
+  };
+
+  return [
+    assetLoader,
+    scssLoader,
+    //  tsLoader,
+    babelLoader,
+    svgLoader,
+  ];
 }
