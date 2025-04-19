e1748977d14c245e5959c487be9f372ff1f62e77
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-04-19 20:28:43 +0300

    fox1

--- config/build/babel/buildBabelLoader.ts
+++ config/build/babel/buildBabelLoader.ts
@@ -1,7 +1,20 @@
-import { BuildOptions } from "../types/types";
+import {BuildOptions} from "../types/types";
+// import {removeDataTestIdBabelPlugin} from "./removeDataTestIdBabelPlugin";
 
-export function buildBabelLoader({ mode }: BuildOptions) {
-  const isDev = mode === "development";
+export function buildBabelLoader({mode}: BuildOptions) {
+  const isDev = mode === 'development';
+  const isProd= mode === 'production';
+
+  // const plugins = [];
+
+  // if(isProd) {
+  //   plugins.push([
+  //     removeDataTestIdBabelPlugin,
+  //     {
+  //       props: ['data-testid']
+  //     }
+  //   ])
+  // }
 
   return {
     test: /\.tsx?$/,
@@ -10,16 +23,17 @@ export function buildBabelLoader({ mode }: BuildOptions) {
       loader: "babel-loader",
       options: {
         presets: [
-          "@babel/preset-env",
+          '@babel/preset-env',
           "@babel/preset-typescript",
           [
             "@babel/preset-react",
             {
-              runtime: isDev ? "automatic" : "classic",
-            },
-          ],
+              runtime: isDev ? 'automatic' : 'classic',
+            }
+          ]
         ],
-      },
-    },
-  };
+        // plugins: plugins.length ? plugins : undefined,
+      }
+    }
+  }
 }
